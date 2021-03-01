dcf514d24b7488bbb03e8a048b8fcbdf42ef5709
palepanorama <matthew.keith.thomas@gmail.com>
2021-03-01 17:25:45 -0600

    commit tree e77cb69cc1512b57c5fc9da0270f4d0946824c1c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const testVar = {}
 
+function testFunc() {
+  return "hi"
+}
+
+
+function superbowlWin(array) {
+  let winObj = array.find( function(e) { return e.result === "W"} )
+  if (winObj === undefined) {
+    return winObj
+  } else {
+    return winObj.year
+  }
+} 
